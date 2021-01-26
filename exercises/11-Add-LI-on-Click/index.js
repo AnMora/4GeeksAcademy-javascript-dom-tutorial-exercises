9d7c3a9104a920bd3a29c418e411c4441ccdaa5e
Anmora96 <anmora.2396@gmail.com>
2021-01-26 22:46:09 +0000

    Ejercicio 12

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,6 @@
-let button = document.querySelector("#superDuperButton");
+let button = document.getElementById("superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	var elem1 = document.createElement("LI");
+	document.querySelector("#myList").appendChild(elem1);
 });
---
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -0,0 +1,4 @@
+let mystring = "<p>Hello!</p> <strong>My friend</strong>";
+document.write(mystring);
+
+// Esto estaba en el HTML
