7115725c32eb2cea49ff5d9ab385962ab1bd5fef
Anmora96 <anmora.2396@gmail.com>
2021-01-26 21:49:10 +0000

    Ejercicio 11

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
-
+	var elem1 = document.createElement("P");
+	elem1.innerHTML = "Hello World";
+	elem1.style.background = "yellow";
+	document.body.appendChild(elem1);
 });
