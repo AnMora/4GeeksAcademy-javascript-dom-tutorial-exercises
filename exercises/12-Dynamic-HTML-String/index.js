f1a342a14d386bf2444aef5bffd723874e7c2f4f
Anmora96 <anmora.2396@gmail.com>
2021-01-26 23:26:53 +0000

    Ejercicio 13

--- exercises/12-Dynamic-HTML-String/index.js
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -1,4 +1,6 @@
-let mystring = "<p>Hello!</p> <strong>My friend</strong>";
-document.write(mystring);
+var myDate = new Date();
+var myYear = myDate.getFullYear();
+let mystring = "<p>Hello!</p><p><strong>My friend</strong>, we are in the year";
+document.write(mystring + " " + myYear + "</p>");
 
 // Esto estaba en el HTML
