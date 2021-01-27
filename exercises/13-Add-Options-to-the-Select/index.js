faa60edd69329f13b7883bce6f2d2cebd7859371
Anmora96 <anmora.2396@gmail.com>
2021-01-27 06:48:42 +0000

    Avance de Ejercicio 13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,6 +1,23 @@
 window.onload = function() {
-	console.log("The website was loaded...");
+	console.log("The website was loaded...cambio");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+	for (var i = 0; i < countries.length; i++) {
+		let valores = document.createElement("option");
+		valores.innerHTML = countries;
+		document.getElementById("mySelect").appendChild(valores);
+	}
+	/*
+    countries.forEach(function(countries) {
+		console.log(countries);
+	});
+    */
+	//mySelect
+
+	/*
+    var elem1 = document.createElement("P");
+	elem1.innerHTML = "Hello World";
+	document.body.appendChild(elem1);
+    */
 };
